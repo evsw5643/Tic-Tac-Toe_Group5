b9bf8848f78c6a73aed122a952c6611dc8e40331
Evan Swett <evsw5643@colorado.edu>
2021-09-23 16:41:29 -0600

    Updated CreateBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
-void CreateBoard()
+int ** CreateBoard()
 {
-    int arr[3][3] = {};
+    int ** arr = 0;
+
+    for(int i = 0 ; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+    return arr;
 }
 
 int main() {
