1aa9ed5bc7d969e206ef22d7ac35fcd80f7499a5
19jmatsko <joma4587@colorado.edu>
2021-09-24 21:59:22 +0000

    PlaceMaker and updated DisplayBoard

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #Tic-Tac-Toe_Group5#
+
+Person 1 -- Evan Swett
+Person 2 -- Jonathan Matsko
+
+This program runs a two player tic tac toe game in the terminal.
--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 
-std::vector< std::vector<char> > board; 
+std::vector< std::vector<char> > board;
+int turn_count = 1;
 
 std::vector< std::vector<char> > CreateBoard()
 {
@@ -19,14 +20,40 @@ std::vector< std::vector<char> > CreateBoard()
 void DisplayBoard() {
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 3; j++) {
-            std::cout<<"| "<<board[i][j]<<" |";
+            std::cout<<" "<<board[i][j]<<" ";
         }
 
-        std::cout<<std::endl;
+        std::cout<<std::endl<<std::endl;
     }
+
+    std::cout<<std::endl;
+}
+
+void PlaceMaker(std::vector<int> location) {
+    int x = location[0] - 1;
+    int y = location[1] - 1;
+    
+    if(turn_count % 2 != 0)
+    {
+        board[x][y] = 'X';
+    }
+    else {
+        board[x][y] = 'O';
+    }
+    
+    DisplayBoard();
 }
 
 int main() {
     board = CreateBoard();
     DisplayBoard();
+    
+    std::vector<int> v = {1, 2};
+    std::vector<int> v2 = {2, 3};
+    
+    PlaceMaker(v);
+
+    turn_count++;
+
+    PlaceMaker(v2);
 }
